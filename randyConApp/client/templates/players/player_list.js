442a0870b895a66dc99e3ece67df7c862e060fbf
Carl Vereen <carlvereen@gmail.com>
2015-07-28 23:07:36 -0600

    created player submission form

--- randyConApp/client/templates/players/player_list.js
+++ randyConApp/client/templates/players/player_list.js
@@ -4,3 +4,9 @@ Template.playersList.helpers({
   return Players.find({}, {sort: {submitted: -1}});
   }
 });
+
+Template.playersList.events({
+  'click .btn': function () {
+    Router.go('/playersubmit');
+  }
+});
