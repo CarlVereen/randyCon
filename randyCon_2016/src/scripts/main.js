ae72cd421cb1b8620938c40e5f028f125727b1d9
Carl Vereen <carlvereen@gmail.com>
2016-01-16 09:26:59 -0700

    Setup meteor app layout

--- randyCon_2016/src/scripts/main.js
+++ randyCon_2016/src/scripts/main.js
@@ -21,3 +21,30 @@ function MyViewModel() {
 
 viewModel = new MyViewModel();
 ko.applyBindings(viewModel);
+
+function onSuccess(googleUser) {
+      var sgnOut = document.getElementById("signLink");
+      sgnOut.innerHTML = sgnOut.innerHTML('<a href="#" onclick="signOut();">Sign out</a>');
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'https://www.googleapis.com/auth/plus.login',
+        'width': 200,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+      }
